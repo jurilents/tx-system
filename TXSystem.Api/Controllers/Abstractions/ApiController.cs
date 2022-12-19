207a7c509ae35ad906ee5c605b9b0cf65a9d99d2
jurilents <keeeneeek@gmail.com>
2022-12-20 00:24:30 +0200

    Drop old API project

--- TXSystem.Api/Controllers/Abstractions/ApiController.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-
-namespace TXSystem.Api.Controllers.Abstractions;
-
-[ApiController]
-[Produces("application/json")]
-[Route("api/[controller]")]
-public class ApiController : ControllerBase { }
--- TXSystem.Api/README.md
+++
@@ -1,3 +0,0 @@
-# TaxesSystem – Yermakov Yurii's Course Work
-
-Alternative documentation – [ReDoc](/docs-{version}).
